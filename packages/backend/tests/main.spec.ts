9ce609185fb33d84d3baad94792907148976dbf1
Tomek Buszewski <tomek.buszewski@gmail.com>
2022-04-28 22:45:13 +0200

    test(backend): basic api tests

--- packages/backend/tests/main.spec.ts
+++ packages/backend/tests/main.spec.ts
@@ -1,13 +1,45 @@
 import * as supertest from "supertest";
 import { app } from "../src/main";
 
+import { cities, companies, specialities } from "@construkt/mock-db";
+
 describe("[API] Main methods", () => {
   describe("[GET]", () => {
-    it("properly returns hello message", async () => {
-      const response = await supertest(app).get("/api");
+    it("properly lists companies", async () => {
+      const { body } = await supertest(app).get("/api/items");
 
-      expect(response.status).toBe(200);
-      expect(response.body).toMatchObject({ message: "Welcome to backend!" });
+      expect(body).toMatchObject(companies);
+    });
+
+    it("should properly filter companies basing on cities", async () => {
+      const city = cities[0].id;
+      const { body } = await supertest(app).get("/api/items").query({ city });
+
+      expect(body).toMatchObject(
+        companies.filter((item) => item.city.id === city),
+      );
+    });
+
+    it("should properly filter companies basing on specialities", async () => {
+      const spec1 = specialities[0].id;
+      const spec2 = specialities[1].id;
+      const { body } = await supertest(app)
+        .get("/api/items")
+        .query({ specialities: [spec1, spec2] });
+
+      expect(body).toMatchObject(
+        companies.filter(({ specialities }) =>
+          specialities.find(({ id }) => id === spec1 || id === spec2),
+        ),
+      );
+    });
+
+    it("should return empty collection when filtering criteria are not met", async () => {
+      const { body } = await supertest(app)
+        .get("/api/items")
+        .query({ city: "non-existent-id" });
+
+      expect(body).toMatchObject([]);
     });
   });
 });
--- packages/mock-db/src/index.ts
+++ packages/mock-db/src/index.ts
@@ -1,5 +1,93 @@
-export default [
+export const specialities = [
+  { id: "J7mNqwPwAUg5hun", name: "plumbing" },
+  { id: "yK5qDaGVy-h0OUH", name: "joinery" },
+  { id: "QNEnplB2mVv-owo", name: "locksmith" },
+  { id: "mtFgIke0drwcDkv", name: "excavations" },
+  { id: "Z_6G6mgLzoIN_AH", name: "retrieval of weird items from my fridge" },
+];
+
+export const cities = [
+  { id: "R4jQw", name: "new york" },
+  { id: "7tSJt", name: "new jersey" },
+  { id: "UNG9g", name: "boston" },
+];
+
+export const companies = [
   {
-    name: "asd",
+    id: "zSnLHenR2t",
+    name: "company-a",
+    logo: "https://www.fillmurray.com/200/120",
+    city: cities[2],
+    specialities: [specialities[2], specialities[1]],
+  },
+  {
+    id: "zSnLHenR2t",
+    name: "company-b",
+    logo: "https://www.fillmurray.com/200/120",
+    city: cities[0],
+    specialities: [specialities[1]],
+  },
+  {
+    id: "PovEXTDG5u",
+    name: "company-c",
+    logo: "https://www.fillmurray.com/200/120",
+    city: cities[1],
+    specialities: [specialities[1], specialities[0]],
+  },
+  {
+    id: "YTLW-Lugdt",
+    name: "company-d",
+    logo: "https://www.fillmurray.com/200/120",
+    city: cities[2],
+    specialities: [specialities[1]],
+  },
+  {
+    id: "y__bONSB-W",
+    name: "company-e",
+    logo: "https://www.fillmurray.com/200/120",
+    city: cities[1],
+    specialities: [specialities[1]],
+  },
+  {
+    id: "wKTZxnDNqX",
+    name: "company-f",
+    logo: "https://www.fillmurray.com/200/120",
+    city: cities[0],
+    specialities: [specialities[2]],
+  },
+  {
+    id: "EGWC8WPOoM",
+    name: "company-g",
+    logo: "https://www.fillmurray.com/200/120",
+    city: cities[1],
+    specialities: [specialities[1]],
+  },
+  {
+    id: "He88J425or",
+    name: "company-h",
+    logo: "https://www.fillmurray.com/200/120",
+    city: cities[2],
+    specialities: [specialities[1], specialities[2]],
+  },
+  {
+    id: "GwauhNFWTQ",
+    name: "company-i",
+    logo: "https://www.fillmurray.com/200/120",
+    city: cities[1],
+    specialities: [specialities[1]],
+  },
+  {
+    id: "5L0ejyfwnq",
+    name: "company-j",
+    logo: "https://www.fillmurray.com/200/120",
+    city: cities[1],
+    specialities: [specialities[1], specialities[0]],
+  },
+  {
+    id: "9sdQS4y8bi",
+    name: "company-k",
+    logo: "https://www.fillmurray.com/200/120",
+    city: cities[1],
+    specialities: [specialities[0]],
   },
 ];
