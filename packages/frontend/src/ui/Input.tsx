53a3e163fb51c138c71b7ea2a8d882f018647459
Tomek Buszewski <tomek.buszewski@gmail.com>
2022-05-02 18:43:16 +0200

    style(frontend): functions cleanup

---
+++ packages/frontend/src/ui/Input.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+  border-radius: 4rem;
+  height: 4rem;
+  line-height: 4rem;
+  padding: 0 2rem;
+
+  border: 0.125rem solid ${({ theme }) => theme.colors.fadedBackground};
+`;
