d059e88ba2da33b09e23d241a82567f8feb2729b
Tomek Buszewski <tomek.buszewski@gmail.com>
2022-05-02 12:04:15 +0200

    feat(backend): cors middleware

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -2,12 +2,14 @@ import * as express from "express";
 import { getCities, getCompanies, getSpecialities, ROUTES } from "./routes";
 
 import helmet from "helmet";
+import { corsMiddleware } from "./middleware/cors";
 
 export const app = express();
 
 const router = express.Router();
 
 app.use(helmet());
+app.use(corsMiddleware);
 app.use(ROUTES.API_MAIN, router);
 
 router.route(ROUTES.COMPANIES).get(getCompanies);
@@ -15,7 +17,7 @@ router.route(ROUTES.CITIES).get(getCities);
 router.route(ROUTES.SPECIALITIES).get(getSpecialities);
 
 if (process.env.NODE_ENV !== "test") {
-  const port = process.env.BACKEND_PORT || 3333;
+  const port = process.env.NX_BACKEND_PORT || 3333;
   const server = app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}${ROUTES.API_MAIN}`);
   });
