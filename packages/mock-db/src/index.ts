270c7a0c4159c1b139e2b6a80188b2fddbb794a4
Tomek Buszewski <tomek.buszewski@gmail.com>
2022-05-02 13:25:47 +0200

    fix(mock-db): fixed unique id for items

--- packages/mock-db/src/index.ts
+++ packages/mock-db/src/index.ts
@@ -21,7 +21,7 @@ export const companies = [
     specialities: [specialities[2], specialities[1]],
   },
   {
-    id: "zSnLHenR2t",
+    id: "zSnLHenR2x",
     name: "company-b",
     logo: "https://www.fillmurray.com/200/120",
     city: cities[0],
